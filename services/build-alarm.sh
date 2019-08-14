799043ce17d1293e6c9197c639af3522fbe1ae32
Kunal Shroff <kunalshroff9@gmail.com>
2019-08-14 10:52:55 -0400

    Update build-alarm.sh

--- services/build-alarm.sh
+++ services/build-alarm.sh
@@ -5,21 +5,21 @@ export TOP="$PWD"
 
 # download elastic
 if [ ! -d ${TOP}/lib/elasticsearch-6.3.1 ]; then
-    wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.3.1.tar.gz -O /tmp/elasticsearch-6.3.1.tar.gz
+    wget --no-check-certificate https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.3.1.tar.gz -O /tmp/elasticsearch-6.3.1.tar.gz
     tar xzvf /tmp/elasticsearch-6.3.1.tar.gz --directory ${TOP}/lib
     rm /tmp/elasticsearch-6.3.1.tar.gz
 fi
 
 # download kibana
 if [ ! -d ${TOP}/lib/kibana-6.3.1 ]; then
-    wget wget https://artifacts.elastic.co/downloads/kibana/kibana-6.3.1-linux-x86_64.tar.gz -O /tmp/kibana-6.3.1-linux-x86_64.tar.gz
+    wget --no-check-certificate https://artifacts.elastic.co/downloads/kibana/kibana-6.3.1-linux-x86_64.tar.gz -O /tmp/kibana-6.3.1-linux-x86_64.tar.gz
     tar xzvf /tmp/kibana-6.3.1-linux-x86_64.tar.gz --directory ${TOP}/lib
     rm /tmp/kibana-6.3.1-linux-x86_64.tar.gz
 fi
 
 # download kafka
 if [ ! -d ${TOP}/lib/kafka_2.11-2.1.0 ]; then
-    wget https://archive.apache.org/dist/kafka/2.1.0/kafka_2.11-2.1.0.tgz -O /tmp/kafka_2.11-2.1.0.tgz
+    wget --no-check-certificate https://archive.apache.org/dist/kafka/2.1.0/kafka_2.11-2.1.0.tgz -O /tmp/kafka_2.11-2.1.0.tgz
     tar xzvf /tmp/kafka_2.11-2.1.0.tgz --directory ${TOP}/lib
     rm /tmp/kafka_2.11-2.1.0.tgz
 fi
