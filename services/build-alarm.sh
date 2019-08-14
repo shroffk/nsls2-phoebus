0e93adcd6f10e937e8054056ee9d931b6b48b778
Kunal Shroff <kunalshroff9@gmail.com>
2019-08-14 10:45:19 -0400

    Update build-alarm.sh

--- services/build-alarm.sh
+++ services/build-alarm.sh
@@ -6,21 +6,21 @@ export TOP="$PWD"
 # download elastic
 if [ ! -d ${TOP}/lib/elasticsearch-6.3.1 ]; then
     wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.3.1.tar.gz -O /tmp/elasticsearch-6.3.1.tar.gz
-    tar xzvf /tmp/elasticsearch-6.3.1.tar.gz --directory $TOP
+    tar xzvf /tmp/elasticsearch-6.3.1.tar.gz --directory ${TOP}/lib
     rm /tmp/elasticsearch-6.3.1.tar.gz
 fi
 
 # download kibana
 if [ ! -d ${TOP}/lib/kibana-6.3.1 ]; then
     wget wget https://artifacts.elastic.co/downloads/kibana/kibana-6.3.1-linux-x86_64.tar.gz -O /tmp/kibana-6.3.1-linux-x86_64.tar.gz
-    tar xzvf /tmp/kibana-6.3.1-linux-x86_64.tar.gz --directory $TOP
+    tar xzvf /tmp/kibana-6.3.1-linux-x86_64.tar.gz --directory ${TOP}/lib
     rm /tmp/kibana-6.3.1-linux-x86_64.tar.gz
 fi
 
 # download kafka
 if [ ! -d ${TOP}/lib/kafka_2.11-2.1.0 ]; then
     wget http://mirrors.sonic.net/apache/kafka/2.1.0/kafka_2.11-2.1.0.tgz -O /tmp/kafka_2.11-2.1.0.tgz
-    tar xzvf /tmp/kafka_2.11-2.1.0.tgz --directory $TOP
+    tar xzvf /tmp/kafka_2.11-2.1.0.tgz --directory ${TOP}/lib
     rm /tmp/kafka_2.11-2.1.0.tgz
 fi
 
