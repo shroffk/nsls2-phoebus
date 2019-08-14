7a2c60b99cf72b50611ee5c5332bc60fa6da1000
Kunal Shroff <kunalshroff9@gmail.com>
2019-08-14 10:42:00 -0400

    Create build-alarm.sh

---
+++ services/build-alarm.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# should point to the nsls2-phoebus installation location
+export TOP="$PWD"
+
+# download elastic
+if [ ! -d ${TOP}/lib/elasticsearch-6.3.1 ]; then
+    wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.3.1.tar.gz -O /tmp/elasticsearch-6.3.1.tar.gz
+    tar xzvf /tmp/elasticsearch-6.3.1.tar.gz --directory ~/epics-tools/services
+    rm /tmp/elasticsearch-6.3.1.tar.gz
+fi
+
+# download kibana
+if [ ! -d ${TOP}/lib/kibana-6.3.1 ]; then
+    wget wget https://artifacts.elastic.co/downloads/kibana/kibana-6.3.1-linux-x86_64.tar.gz -O /tmp/kibana-6.3.1-linux-x86_64.tar.gz
+    tar xzvf /tmp/kibana-6.3.1-linux-x86_64.tar.gz --directory ~/epics-tools/services
+    rm /tmp/kibana-6.3.1-linux-x86_64.tar.gz
+fi
+
+# download kafka
+if [ ! -d ${TOP}/lib/kafka_2.11-2.1.0 ]; then
+    wget http://mirrors.sonic.net/apache/kafka/2.1.0/kafka_2.11-2.1.0.tgz -O /tmp/kafka_2.11-2.1.0.tgz
+    tar xzvf /tmp/kafka_2.11-2.1.0.tgz --directory ~/epics-tools/services
+    rm /tmp/kafka_2.11-2.1.0.tgz
+fi
+
