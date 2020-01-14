626f27161ebcc70fbffd559dd0884ee581f10631
Kunal Shroff <kunalshroff9@gmail.com>
2020-01-14 17:03:48 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ mkdir -p ${TOP}/lib/jvm
 
 # download jdk 11
 if [ ! -d ${TOP}/lib/jvm/jdk-11.0.2 ]; then
-    wget https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz -O /tmp/openjdk-11+28_linux-x64_bin.tar.gz
+    wget --no-verbose https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz -O /tmp/openjdk-11+28_linux-x64_bin.tar.gz
     tar xfvz /tmp/openjdk-11+28_linux-x64_bin.tar.gz --directory ${TOP}/lib/jvm
     rm /tmp/openjdk-11+28_linux-x64_bin.tar.gz
 fi
@@ -17,7 +17,7 @@ fi
 
 # download maven
 if [ ! -d ${TOP}/lib/apache-maven-3.6.0 ]; then
-    wget https://archive.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz -O /tmp/apache-maven-3.6.0-bin.tar.gz
+    wget --no-verbose https://archive.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz -O /tmp/apache-maven-3.6.0-bin.tar.gz
     tar xzvf /tmp/apache-maven-3.6.0-bin.tar.gz --directory ${TOP}/lib
     rm /tmp/apache-maven-3.6.0-bin.tar.gz
 fi
@@ -41,11 +41,11 @@ export PATH="$MVN_HOME/bin:$PATH"
 
 #build phoebus
 cd $TOP/lib/phoebus
-mvn clean install --settings=$TOP/nsls2-phoebus/settings.xml -DskipTests=true -q -l /tmp/phoebus-nsls.log
+mvn clean install --settings=$TOP/nsls2-phoebus/settings.xml -DskipTests=true
 
 #build nsls2 product
 cd $TOP/nsls2-phoebus/products
-mvn clean install --settings=$TOP/nsls2-phoebus/settings.xml -DskipTests=true -q -l /tmp/phoebus-nsls.log
+mvn clean install --settings=$TOP/nsls2-phoebus/settings.xml -DskipTests=true
 
 # download nsls2 specific configurations 
 
