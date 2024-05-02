5f314b9ce14d459ba19bac9586c1b5bd859d628f
Kunal Shroff <kunalshroff9@gmail.com>
2024-05-02 12:17:14 -0400

    Prepare for Phoebus 4.7.4 release

--- run-phoebus.sh
+++ run-phoebus.sh
@@ -7,7 +7,7 @@ export JAVA_HOME=${TOP}/lib/jvm/jdk-17
 export PATH="$JAVA_HOME/bin:$PATH"
 
 echo $TOP
-V="4.6.4-SNAPSHOT"
+V="4.7.4-SNAPSHOT"
 
 # figure out the path to the product jar
 if [[ -z "${PHOEBUS_JAR}" ]]; then
